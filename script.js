e22d86b2ea5eb4e9fb13cc7aa17c5bd8caa6a6a2
John Pavley <john@pavley.com>
2022-03-23 18:54:38 -0400

    seconds tring formatting

--- script.js
+++ script.js
@@ -51,6 +51,10 @@ function drawLocalClock() {
     const period = hours >= 12 ? 'PM' : 'AM';
     let hour = hours % 12;
 
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
